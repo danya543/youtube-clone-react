831e016239e9876dc6e4deea021c765ad3eb97f6
Daniil <dgilev75@gmail.com>
2025-04-24 16:45:07 +0300

    feat: add styles for aside block and cards

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,13 +1,17 @@
 import { CardProps } from "@src/types/types";
 
+import styles from './Card.module.scss'
+
 export const Card = (props: CardProps) => {
     const { header, img, author, views, duration } = props;
     return (
-        <div className={''}>
-            <img src={img} alt="poster" />
-            <p>{duration}</p>
+        <div className={styles.container}>
+            <div className={styles.poster}>
+                <img src={img} alt="poster" />
+                <p>{duration}</p>
+            </div>
             <h3>{header}</h3>
-            <div>
+            <div className={styles.info}>
                 <p>{views}k views</p>
                 <p>{author}</p>
             </div>
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,16 @@
 import { Card } from "@components/Card/Card";
 import { CardsInfo } from "@components/constants";
+import { Button } from "@ui/Button";
+
+import styles from './Sidebar.module.scss'
 
 export const Sidebar = () => {
     return (
         <aside>
+            <div className={styles.header}>
+                <h3>Next</h3>
+                <p>Autoplay<Button /></p>
+            </div>
             {CardsInfo.map((item, index) => <Card author={item.author} views={item.views} img={item.img} header={item.header} duration={item.duration} key={index} />)}
         </aside>
     )
